960ff61ad1f4ba8a2648745f7c5af7a1d926a1f6
1GamerDev <loftynine7+1gdgithub@hotmail.com>
2018-04-22 19:06:35 +0100

    Version 1.0.0
    Recommended length is higher than 0 and less than 21.

---
+++ random.js
@@ -0,0 +1,26 @@
+function randomNumber(len = 0) {
+  if (len <= 0 || len == null) {
+    console.error("Invalid length.");
+    return false;
+  }
+  if (typeof len != "number") {
+    return false;
+  }
+  if (len.toString().includes(".")) {
+    return false;
+  }
+  var returnValue = "";
+  var loop = true;
+  function returnRandomNumberAsString() {
+    var returnInteger = Math.floor(Math.random() * 10);
+    return returnInteger.toString();
+  }
+  while (loop) {
+    if (returnValue.length != len) {
+      returnValue = parseInt(returnRandomNumberAsString() + returnValue).toString();
+    } else {
+      loop = false;
+    }
+  }
+  return parseInt(returnValue);
+}
